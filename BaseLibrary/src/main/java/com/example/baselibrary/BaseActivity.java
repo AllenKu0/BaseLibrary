fab764afca12c59bfe99c0cd345be31fe613677a
AllenKu0 <eacf2469@gmail.com>
2022-07-11 18:09:22 +0800

    試推

--- BaseLibrary/src/main/java/com/example/baselibrary/BaseActivity.java
+++ BaseLibrary/src/main/java/com/example/baselibrary/BaseActivity.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity {
 
     public void showToast(Context context,String msg){
         Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
